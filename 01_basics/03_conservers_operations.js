76d94b994cf801d1695bffa02ad6045cd001690e
Ansh Suneja <35730349+anshsuneja147@users.noreply.github.com>
2023-09-21 18:52:36 +0000

    conversions_operations

---
+++ 01_basics/03_conservers_operations.js
@@ -0,0 +1,32 @@
+let score = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log( typeof valueInNumber);
+// console.log(valueInNumber); // this will give NaN output
+let toString = String(score)
+// console.log(typeof toString);
+// console.log(toString);
+
+let result = 45 + toString; // this ill change the result to string
+// console.log(result);
+// console.log(typeof result);
+
+// console.log(2/3); // 0.6666
+// console.log(2%3); // 2 , this gives the reminder
+
+let str1 = "hello"
+let str2 = " Ansh"
+let str3 = str1 + str2; // if we use - sign then it will give NaN as an output
+// console.log(str3);
+
+console.log("1" + 2); // output 12
+console.log(1 + "2"); // output 12
+console.log("1" + "2"); // output 12
+console.log("1" + 2 + 2); // output 122
+console.log(1 + 2 + "2"); // output 32
+
+
+
