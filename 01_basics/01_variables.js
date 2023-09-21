5a8dac4b97a32eacc5d37c17199f21845af47da5
Ansh Suneja <35730349+anshsuneja147@users.noreply.github.com>
2023-09-21 17:58:42 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 12432
+let accountEmail = "anshsuneja147@gmail.com"
+var accountPassord = "763927"
+accountCity = "Abohar"
+
+// accountID = 34 not allowed
+
+/* Prefer not to use var
+because of issue in block scope and functionla scope*/
+
+accountEmail = "ansh@gmail.com"
+accountPassord = "3433"
+accountCity = " bangaluru"
+
+console.table([accountID,accountPassord,accountEmail,accountCity])
