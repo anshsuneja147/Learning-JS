ea370eaa032ea39c639f712e727de90b83b5f55c
Ansh Suneja <35730349+anshsuneja147@users.noreply.github.com>
2023-09-21 17:58:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all code as new JS version
+//  alert(3 + 3); we are using node js not browser , so it will not work here
+
+console.log("hi there");
+
+// number => 2 to the power 53;
+// BigInt => we use if number is big/large
+// string => prefer double-qoute " "
+// boolean => true/false
+// null => standalone value
+// undefined 
+//symbol => unique
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
